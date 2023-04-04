864fab4198767e9a4e1711a549720fae4ac15893
grauziitisos <85872382+grauziitisos@users.noreply.github.com>
2023-04-04 20:31:37 +0300

    tree

--- 1pr/main.py
+++ 1pr/main.py
@@ -6,20 +6,26 @@ class node:
     def __init__(self):
         self.parents = []
         self.children = []
+
+class tree:
+    struct: dict[int, dict[int, node]]
+    def __init__(self):
+        self.struct = {}
         
-#oh no cannot define a method inside class that returns a list of this class objects?        
-def populate(st: node) -> list[node]:
-        ans = list[node]()
-        ans.append(st)
+#oh no cannot define a method inside class (node) that returns a list of this class objects?        
+def populate(st: node) -> tree:
+        ans = tree()
+        ans.struct[1] = {1: st}
         return ans
     
 def main():
     n = node()
     n.letter = "A"
     n.status="110010"
-    lst = populate(n)
-    for nd in lst:
-        print(nd.letter)
+    thetree = populate(n)
+    for nd, kv in thetree.struct.items():
+        for k, v in kv.items():
+            print(v.letter)
     print(n.letter)
     
 if __name__ == "__main__":
